3ce93f4d578ada4c8c461cec8b5fa8ab29cd1fa1
Maikol2503 <104582495+Maikol2503@users.noreply.github.com>
2022-05-29 17:13:10 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,23 @@
+
+//animacion de la barra de menu
+document.querySelector(".bars__menu").addEventListener("click", animateBars );
+let line1__bars = document.querySelector(".line1__bars-menu");
+let line2__bars = document.querySelector(".line2__bars-menu");
+let line3__bars = document.querySelector(".line3__bars-menu");
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu")
+    line2__bars.classList.toggle("activeline2__bars-menu")
+    line3__bars.classList.toggle("activeline3__bars-menu")
+}
+
+//menu visible
+const botonBar = document.querySelector(".bars__menu")
+const item = document.querySelector(".menu-menu")
+botonBar.addEventListener("click" , () => {
+    item.classList.toggle("visible")
+})
+
+
 window.addEventListener("load" , () =>{
 
   const display = document.querySelector(".calculadora-display");
